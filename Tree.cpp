bc72f115fea01ffe4ce9a2d38710b82f8784773b
hartleyleo <78177307+hartleyleo@users.noreply.github.com>
2021-11-23 12:06:32 -0500

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -23,3 +23,27 @@ Node* Tree::insert ( int data ) {
 
     // This is the public insert
 }
+
+
+Node* Tree::rotateLeft ( Node* root ) {
+
+    node *child = root->right;
+    node *childLeft = child->left;
+
+    child->left = root;
+    root->right = childLeft;
+
+    return child;
+}
+
+
+Node* rotateRight ( Node* root ) {
+
+    node *child = root->left;
+    node *childRight =  child->right;
+
+    child->right = root;
+    root->left = childRight;
+
+    return child;
+}
