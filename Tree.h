3615173935e0174035df5838f5cc9e4c4154d94a
hartleyleo <78177307+hartleyleo@users.noreply.github.com>
2021-12-05 15:51:17 -0500

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -15,8 +15,15 @@ class Tree {
 
         Node* rotateLeft ( Node* root );
         Node* rotateRight ( Node* root );
+        bool color ( Node* root );
 
         bool SearchFor ( std::string data, Node* root );
+        int Count ( Node* root, std::string key );
+
+        int height ( Node* root );
+        void preorder ( Node* root );
+        void inorder ( Node* root );
+        void postorder ( Node* root );
 
     public:
 
@@ -28,5 +35,9 @@ class Tree {
         // Functions
         void Insert ( std::string data );
         bool Search ( std::string data );
+        int Count ( std::string data );
+        int height ();
+
+        void generateDOT ();
 
 };
