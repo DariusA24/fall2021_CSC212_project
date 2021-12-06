2d5cb4488a2043685cde411b2c4739fa4b266428
hartleyleo <78177307+hartleyleo@users.noreply.github.com>
2021-12-05 23:52:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@
 //         `--'   7//\    ///\                                          | |
 
 // Function declarations
-void populateTree ( std::string fileName, Tree* tree );
+void populateTree ( std::string fileName, Tree* tree, int populationMethod );
 int getUserChoice ();
 bool handleUserChoice ( Tree* tree, int userChoice );
 
@@ -34,12 +34,38 @@ int main ( int argc, char *argv[] ) {
     // Make tree here
     Tree *inputFileBSTree = new Tree();
 
+    // Get method of population
+    int populationMethod;
+    std::cout << "Please choose tree population method:\n(1.) Allow Duplicate nodes\n(2.) Unique nodes only\nAllowing duplicate nodes makes for a disfunction .dot visualization of the tree" << std::endl;
+    std::cin >> populationMethod;
+
+    // handle input to make sure legal input is entered;
+    bool satisfied = false;
+    while ( !( satisfied ) ) {
+
+        // if the method is a legal method allow the user input
+        if ( populationMethod == 1 || populationMethod == 2 ) {
+
+            satisfied = true;
+
+        } else {
+
+            // If they enter a illegal input then force them to retry
+            std::cout << "Please input a legal number, 1 or 2 for your chosen population method respectively" << std::endl;
+            std::cin >> populationMethod;
+
+        }
+
+    }
+
     // Clock used to see how long it took to populate the tree from the users file
     clock_t t;
 
+    // Populate tree based off of method
     t = clock();
-    populateTree( fileName, inputFileBSTree );
+    populateTree ( fileName, inputFileBSTree, populationMethod );
     t = clock() - t;
+
     std::cout << "Tree successfully populated, took " << ( ( float ) t ) / CLOCKS_PER_SEC << " seconds" << std::endl;
 
     // User simple GUI
@@ -53,8 +79,16 @@ int main ( int argc, char *argv[] ) {
     return 0;
 }
 
+
+
+
+
+
 // Functions Definitions
 
+// ---------------------------------------
+// Function to get the choice of the user
+// ---------------------------------------
 int getUserChoice () {
 
     int userChoice = 0;
@@ -68,6 +102,9 @@ int getUserChoice () {
 
 }
 
+// ------------------------------------------
+// Function to handle the choice of the user
+// ------------------------------------------
 bool handleUserChoice ( Tree* tree, int userChoice ) {
 
     // Searching for a word in the Tree
@@ -93,15 +130,18 @@ bool handleUserChoice ( Tree* tree, int userChoice ) {
             std::cout << "This word does not exist in the tree" << std::endl;
         }
 
-        return true;
+    }
 
-    } else if ( userChoice == 2 ) {
+    // Finding the height of the tree
+    else if ( userChoice == 2 ) {
 
         // Returns the height of the tree
         std::cout << "Height of tree: " << tree->height() << std::endl;
-        return true;
 
-    } else if ( userChoice == 3 ) {
+    }
+
+    // Generating a .dot file from the tree
+    else if ( userChoice == 3 ) {
 
         // Generate DOT file
         std::string fileName;
@@ -109,17 +149,26 @@ bool handleUserChoice ( Tree* tree, int userChoice ) {
         std::cin >> fileName;
         tree->generateDigraph ( fileName + ".dot" );
         std::cout << fileName << ".dot successfuly generated" << std::endl;
-        return true;
 
-    } else if ( userChoice == 4 ) {
+    }
+
+    // Quitting the program
+    else if ( userChoice == 4 ) {
         return false;
     }
 
+    // Choice from the user is not a legal input
+    else {
+        std::cout << "Input not understood" << std::endl;
+    }
 
+    return true;
 }
 
-
-void populateTree ( std::string fileName, Tree* tree ) {
+// -------------------------------------------------------------------------------------
+// Function to populate the BST with words from a text file based off the chosen method
+// -------------------------------------------------------------------------------------
+void populateTree ( std::string fileName, Tree* tree, int method ) {
 
     // Data to store user input
     std::string line;
@@ -137,16 +186,26 @@ void populateTree ( std::string fileName, Tree* tree ) {
 		// Takes each word from the string stream and puts it into std::string word
 		while ( ss >> word ) {
 
-			// Insert the word into tree here
-			//std::cout << word << std::endl;
-            tree->Insert( word );
+            // Insert the word into tree here based off of method
+            // Allow duplicate node method
+            if ( method == 1 ) {
+
+                tree->Insert( word );
+
+            }
+            // Unique node values only allowed in tree
+            else if ( method == 2 ) {
+
+                if ( !( tree->Search( word ) ) ) {
+
+                    tree->Insert( word );
+
+		}
+
+            }
 
         }
 
-	}
-
-    // Close file
-	inputFile.close();
+    }
 
 }
-
